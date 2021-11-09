684cf561c4e1c5072632d7afd3e3771e85fd25da
KAIIIII <guyyg03546@gmail.com>
2021-11-09 17:26:20 +0700

    Update

---
+++ php203.php
@@ -0,0 +1,11 @@
+<?php
+    function myTest(){
+        static $a = 0;
+        echo $a;
+        $a = $a + 1;
+    }
+    
+    myTest();
+    myTest();
+    myTest();
+?>
---
+++ php205.php
@@ -0,0 +1,12 @@
+<?php
+    class Car{
+        function testCar(){
+            //$msg = "Thailand";
+            $this->model = "UV";
+        }
+
+    }
+
+    $newCar = new Car();
+    echo $newCar->model;
+?>
